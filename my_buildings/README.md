c3ecbccbb27f7403760934466d6c0f5a0ac6e5de
NuthanChandra <nuthanchandra1997@gmail.com>
2019-12-23 09:14:50 +0530

    End of my effort

--- my_buildings/README.md
+++ my_buildings/README.md
@@ -4,4 +4,4 @@ Created by the command: npx create-react-app my_buildings
 
 * Create components under src for different components
 * Create SearchBar component 
-* Next step: Need to implement searchTerm in SearchBar
+* Next step: Load video from youtube(Checkout https://www.npmjs.com/package/react-youtube)
--- my_buildings/src/components/SearchBar.js
+++ my_buildings/src/components/SearchBar.js
@@ -1,14 +1,22 @@
 import React from 'react';
-
+import searchYouTube from 'youtube-api-search';
 class SearchBar extends React.Component {
-    searchTerm = () => {
+    state = {searchTerm: ""}
 
+    onSearchSubmit = (event) => {
+      event.preventDefault();
+      console.log(this.state.searchTerm);
+      const API_KEY = 'AIzaSyDxXx_Y9ftFm-lv0hel3WI0NMFqJTES89g';
+      searchYouTube({ key: API_KEY, term: this.state.searchTerm, maxResults: 6 }, videos => {
+        console.log(videos);
+      });
     }
+
     render(){
         return (
-          <form className="ui form">
+          <form onSubmit={this.onSearchSubmit}  className="ui form">
             <div className="field">
-              <input type="text" onSubmit={this.searchTerm}/>
+              <input type="text" value={this.state.searchTerm} onChange={(e) => this.setState({searchTerm: e.target.value})}/>
             </div>
           </form>
         );
