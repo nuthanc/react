e8d9377e6b45e74114849dcc15316258fe52115e
nuthanc <nuthanc@juniper.net>
2020-05-28 23:13:51 +0530

    Refactoring a Class to a Function

--- seasons-hooks/README.md
+++ seasons-hooks/README.md
@@ -1,3 +1,9 @@
 ### App Overview
 * npm install
 * npm start
+
+### Refactoring a Class to a Function
+* Import useState and useEffect
+* Define new functional component called App and cherry-pick from class based component
+* [](empty array) meaning run only 1 time in useEffect
+* Update setState and the states
--- seasons-hooks/src/index.js
+++ seasons-hooks/src/index.js
@@ -1,33 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
-class App extends React.Component {
-  state = { lat: null, errorMessage: '' };
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({ lat: position.coords.latitude }),
-      (err) => this.setState({ errorMessage: err.message })
+      (position) => setLat(position.coords.latitude),
+      (err) => setErrorMessage(err.message)
     );
-  }
-  componentDidUpdate() {
-    console.log('My component was re-rendered');
+  }, []);
+
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="Please accept location request" />;
   }
 
-  renderContent() {
-    if (this.state.lat && !this.state.errorMessage) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-    return <Spinner message="Please accept location request" />;
-  }
-  render() {
-    return <div className="border red">{this.renderContent()}</div>;
-  }
-}
+  return <div className="border red">{content}</div>;
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
